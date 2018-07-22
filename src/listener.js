c91422999535335a0cea72e85ac24b2bd346a10f
Gil Tayar <gil@tayar.org>
2018-07-22 13:40:07 +0300

    ignore timeout in listener

--- src/listener.js
+++ src/listener.js
@@ -6,28 +6,31 @@ async function listener(env, project, count, wait) {
   const client = new pubsub.v1.SubscriberClient()
 
   for (let i = 0; i < count; ++i) {
-    const start = Date.now()
-    console.log('listening...')
-    const response = await client.pull({
-      subscription: `projects/${project}/subscriptions/${env}_TestGooglePubsubProblemCanDelete_Sub`,
-      maxMessages: 1,
-    })
+    try {
+      const start = Date.now()
+      console.log('listening...')
+      const response = await client.pull({
+        subscription: `projects/${project}/subscriptions/${env}_TestGooglePubsubProblemCanDelete_Sub`,
+        maxMessages: 1,
+      })
 
-    const message = response[0].receivedMessages[0].message.data
-    const ackId = response[0].receivedMessages[0].ackId
+      const message = response[0].receivedMessages[0].message.data
+      const ackId = response[0].receivedMessages[0].ackId
 
-    console.log('listen time (secs): %d', Math.floor((Date.now() - start) / 1000))
-    console.log(message.toString())
+      console.log('listen time (secs): %d', Math.floor((Date.now() - start) / 1000))
+      console.log(message.toString())
 
-    console.log('waiting...')
-    await p(setTimeout)(wait * 1000)
+      console.log('waiting...')
+      await p(setTimeout)(wait * 1000)
 
-    console.log('acking...')
-    await client.acknowledge({
-      subscription: `projects/${project}/subscriptions/${env}_TestGooglePubsubProblemCanDelete_Sub`,
-      ackIds: [ackId],
-    })
+      console.log('acking...')
+      await client.acknowledge({
+        subscription: `projects/${project}/subscriptions/${env}_TestGooglePubsubProblemCanDelete_Sub`,
+        ackIds: [ackId],
+      })
+    } catch (err) {
+      console.error('ignoring error', err)
+    }
   }
 }
-
 module.exports = listener
