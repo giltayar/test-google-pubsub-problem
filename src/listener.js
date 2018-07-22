99b5a4a40abee1012c6ad03b1bfbaa681d317459
Gil Tayar <gil@tayar.org>
2018-07-22 12:49:44 +0300

    show listen time

--- src/listener.js
+++ src/listener.js
@@ -6,6 +6,7 @@ async function listener(env, project, count, wait) {
   const client = new pubsub.v1.SubscriberClient()
 
   for (let i = 0; i < count; ++i) {
+    const start = Date.now()
     console.log('listening...')
     const response = await client.pull({
       subscription: `projects/${project}/subscriptions/${env}_TestGooglePubsubProblemCanDelete_Sub`,
@@ -15,7 +16,9 @@ async function listener(env, project, count, wait) {
     const message = response[0].receivedMessages[0].message.data
     const ackId = response[0].receivedMessages[0].ackId
 
+    console.log('listen time (secs): %d', Math.floor((Date.now() - start) / 1000))
     console.log(message.toString())
+
 
     console.log('waiting...')
     await p(setTimeout)(wait * 1000)
