a7dbea342dca0faf1b6c99ce83cebe150d274cb7
Gil Tayar <gil@tayar.org>
2018-07-22 12:42:07 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# test-google-pubsub-problem
+
+CLI that yada yada ydad
+
+## Installing
+
+* This package is meant to be used as a CLI, so should be installed with `-g`.
+
+```sh
+npm install -g @applitools/test-google-pubsub-problem
+```
+
+## Using it
+
+* To get all the options, do `test-google-pubsub-problem --help`
---
+++ src/listener.js
@@ -0,0 +1,31 @@
+'use strict'
+const {promisify: p} = require('util')
+const pubsub = require('@google-cloud/pubsub')
+
+async function listener(env, project, count, wait) {
+  const client = new pubsub.v1.SubscriberClient()
+
+  for (let i = 0; i < count; ++i) {
+    console.log('listening...')
+    const response = await client.pull({
+      subscription: `projects/${project}/subscriptions/${env}_TestGooglePubsubProblemCanDelete_Sub`,
+      maxMessages: 1,
+    })
+
+    const message = response[0].receivedMessages[0].message.data
+    const ackId = response[0].receivedMessages[0].ackId
+
+    console.log(message.toString())
+
+    console.log('waiting...')
+    await p(setTimeout)(wait * 1000)
+
+    console.log('acking...')
+    await client.acknowledge({
+      subscription: `projects/${project}/subscriptions/${env}_TestGooglePubsubProblemCanDelete_Sub`,
+      ackIds: [ackId],
+    })
+  }
+}
+
+module.exports = listener
